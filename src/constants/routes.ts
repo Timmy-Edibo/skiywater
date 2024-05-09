57fd3ba483a89f839d628337818cfca3d4229bf5
timmy <timothyedibo@gmail.com>
2024-05-09 11:42:25 +0100

    Added task info in app route endpoint

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,6 +3,7 @@ import User from "../routes/routes.User";
 import Auth from "../routes/routes.Auth";
 import FileUpload from "../routes/routes.FileUpload";
 import { RequestContext } from "express-openid-connect";
+import { createSuccessResponse } from "../config/config.error";
 
 
 export default function routing(app: Express) {
@@ -10,12 +11,20 @@ export default function routing(app: Express) {
   app.use("/api/v1/auth", Auth);
   app.use("/api/v1/files", FileUpload);
 
-  // app.get("/", async(req, res) => {
-  //   const oidc = await req.oidc
-  //   const userInfo = oidc.user
-  //   const isAuthenticated = oidc.isAuthenticated() ? "logged in": "logged out"
-  //   console.log(isAuthenticated)
-  //   res.json({user: userInfo})
+  app.get("/", async (req, res) => {
+    // const oidc = await req.oidc
+    // const userInfo = oidc.user
+    // const isAuthenticated = oidc.isAuthenticated() ? "logged in": "logged out"
+    // console.log(isAuthenticated)
 
-  // })
+    const data = {
+      About: 'Skiywater backend task for file upload and management',
+      Version: '1.0.0',
+      API: 'https://bit.ly/skiywater-backend-tasks-docs',
+      Backend: 'https://skiywater.onrender.com',
+      EMR: 'https://lucid.app/lucidchart/823e7c53-bd02-4020-8215-91c91863ff3b/edit?viewport_loc=-808%2C93%2C2246%2C1044%2C0_0&invitationId=inv_9bbb677d-d6c3-469b-b4f2-832af69807fd',
+      Github: 'https://github.com/Timmy-Edibo/skiywater'
+    }
+    res.status(200).json(createSuccessResponse(data))
+  })
 }
